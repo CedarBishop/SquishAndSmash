6b21c93987b044bb2a2e030389d11e3737afbb69
Cedar Bishop <47874454+CedarBishop@users.noreply.github.com>
2019-10-29 12:29:29 +1100

    Created base enemy and player controller

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,17 +2,98 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum EnemyStates { Idle, FollowPlayer, AttackPlayer, Squished, Immobile}
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public EnemyStates enemyState;
+    private int distanceToReact = 5;
+    PlayerController playerController;
+
+    float timeToExpand = 10;
+    float timeToRemobilise = 5;
+
+
     void Start()
     {
-        
+        playerController = FindObjectOfType<PlayerController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        switch (enemyState)
+        {
+            case EnemyStates.Idle:
+                IdleState();
+                break;
+            case EnemyStates.FollowPlayer:
+                FollowPlayerState();
+                break;
+            case EnemyStates.AttackPlayer:
+                AttackPlayerState();
+                break;
+            case EnemyStates.Squished:
+                SquishedState();
+                break;
+            case EnemyStates.Immobile:
+                ImmobileState();
+                break;
+            default:
+                break;
+        }
+    }
+
+    bool FindPlayer()
+    {
+        if (Vector3.Distance(transform.position, playerController.transform.position) < distanceToReact)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    void IdleState()
+    {
+        if (FindPlayer())
+        {
+
+        }
+        else
+        {
+            enemyState = EnemyStates.FollowPlayer;
+        }
+    }
+
+    void FollowPlayerState()
+    {
+
+    }
+
+    void AttackPlayerState ()
+    {
+
+    }
+
+    void SquishedState ()
+    {
+
+    }
+
+    void ImmobileState()
+    {
+
+    }
+
+    public void OnSquished ()
+    {
+        StartCoroutine("CoOnSquished");
+    }
+
+    IEnumerator CoOnSquished ()
+    {
+        enemyState = EnemyStates.Squished;
+        yield return new WaitForSeconds(timeToExpand);
+        enemyState = EnemyStates.Immobile;
+        yield return new WaitForSeconds(timeToRemobilise);
+        enemyState = EnemyStates.Idle;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 5;
+
+    CharacterController controller;
+    Vector3 inputDirection;
+    Vector3 direction;
+
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        GetInput();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        Jump();
+    }
+
+    void GetInput()
+    {
+        inputDirection = new Vector3(Input.GetAxisRaw("Horizontal"),controller.velocity.y, Input.GetAxisRaw("Vertical"));
+        direction = inputDirection.normalized;
+    }
+
+    void Move()
+    {
+        controller.Move(direction * speed * Time.fixedDeltaTime);
+    }
+
+    void Jump()
+    {
+        if (controller.isGrounded && Input.GetButton("Jump"))
+        {
+            direction.y = jumpSpeed;
+        }
+        direction.y -= gravity * Time.fixedDeltaTime;
+        controller.Move(direction * Time.fixedDeltaTime);
     }
 }
