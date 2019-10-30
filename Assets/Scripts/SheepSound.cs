e2323317f81ca7bc65ddde56a281c4a390121123
Ivan Karyakin <48420289+1whywhy1@users.noreply.github.com>
2019-10-30 11:54:19 +1100

    added sheep sound script

---
+++ Assets/Scripts/SheepSound.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepSound : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerController>())
+        {
+            AudioManager.instance.Play("Sheep");
+        }
+    }
+}
