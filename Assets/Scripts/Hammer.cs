f1d980e0d436952357d03fee58ffd09936004bb4
Cedar Bishop <47874454+CedarBishop@users.noreply.github.com>
2019-10-30 12:53:49 +1100

    Created scripts for boss and hammer

---
+++ Assets/Scripts/Hammer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hammer : MonoBehaviour
+{
+    public static event System.Action HitBoss;
+    public void PlaySmashAnim ()
+    {
+
+    }
+
+    public void Impact ()
+    {
+        if (HitBoss != null)
+        {
+            HitBoss();
+        }
+    }
+}
